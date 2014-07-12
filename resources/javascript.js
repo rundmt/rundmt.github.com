9af1b5d2c2534ddbcd0021e72163c86c91dd2606
Derek Tor <derekmuoktor@gmail.com>
2014-07-12 15:18:52 -0700

    initial commit

---
+++ resources/javascript.js
@@ -0,0 +1,9 @@
+// Your Javascript will go here!
+$(document).ready(function(){
+  //when i click copyright id
+  $("#copyright").click(function(){
+    //run this code
+    $(this).css('color', 'purple');
+    $(this).css('border', '1px solid black');
+  })
+})
