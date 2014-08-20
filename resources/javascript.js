3426af31ca5abbf0ed17edb775c07463b55ee045
Derek Tor <derekmuoktor@gmail.com>
2014-08-20 14:20:24 -0700

    theme

--- resources/javascript.js
+++
@@ -1,9 +0,0 @@
-// Your Javascript will go here!
-$(document).ready(function(){
-  //when i click copyright id
-  $("#copyright").click(function(){
-    //run this code
-    $(this).css('color', 'purple');
-    $(this).css('border', '1px solid black');
-  })
-})
